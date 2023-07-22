741b1fa14b5eb8d3763979ec82ac9e46d9f8d937
Ryan <ryanevans332@gmail.com>
2023-07-22 11:18:45 -0700

    pub script

---
+++ publish.ps1
@@ -0,0 +1,17 @@
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+#publish image
+$tag = (git rev-parse --short HEAD)
+cd mtgdiscord
+docker build -t mtgdiscord -f Dockerfile .
+docker tag mtgdiscord docker.io/ryanevans332/mtgdiscord:$tag
+docker push docker.io/ryanevans332/mtgdiscord:$tag
+cd ..
+
+#publish container
+Connect-AzAccount -TenantId 1254c6e5-0d2f-4fbd-81eb-8756981fc06c
+$sub = "dab8a9ed-89a6-482b-86ec-f4d0d468c471"
+$token = ConvertTo-SecureString -String $env:mtgdiscordbot -AsPlainText -Force
+$envvar = New-AzContainerInstanceEnvironmentVariableObject -Name "mtgdiscordbot" -SecureValue $token
+$container = New-AzContainerInstanceObject -name "mtgdiscordcontainer" -Image ryanevans332/mtgdiscord:$tag -RequestCpu 1 -RequestMemoryInGb 1 -EnvironmentVariable $envvar
+New-AzContainerGroup -Name "mtgdiscordcontainer" -ResourceGroupName "mtgdiscord" -Location "West US 3" -Container $container -RestartPolicy "OnFailure"
